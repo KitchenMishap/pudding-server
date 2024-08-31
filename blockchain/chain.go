0ff3904d8612af030acd11145092216121bf2b5f
NigelGSharpUK <130097204+KitchenMishap@users.noreply.github.com>
2024-08-31 11:53:41 +0100

    Updated for newer pudding-shed. Two blocks with actual data.

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -3,33 +3,54 @@ package blockchain
 import (
 	"github.com/KitchenMishap/pudding-shed/chainreadinterface"
 	"github.com/KitchenMishap/pudding-shed/chainstorage"
+	"math"
 	"pudding-server/multidag"
+	"strconv"
 )
 
 type ChainReader struct {
-	folder    string
-	chainRead chainreadinterface.IBlockChain
+	folder        string
+	chainRead     chainreadinterface.IBlockChain
+	handleCreator chainreadinterface.IHandleCreator
 }
 
 func NewChainReader(folder string) ChainReader {
 	reader := ChainReader{}
 	reader.folder = folder
-	creator, _ := chainstorage.NewConcreteAppendableChainCreator(folder)
-	appendableChain, _, _ := creator.Open(false)
-	reader.chainRead = appendableChain.GetAsChainReadInterface()
+	creator, _ := chainstorage.NewConcreteAppendableChainCreator(
+		folder,
+		[]string{"time", "mediantime", "difficulty", "strippedsize", "size", "weight"},
+		[]string{"size", "vsize", "weight"},
+		true)
+	readableChain, handleCreator, _ := creator.OpenReadOnly()
+	reader.chainRead = readableChain
+	reader.handleCreator = handleCreator
 	return reader
 }
 
 func (cr *ChainReader) GetBlockchainVertex() multidag.Vertex {
 	vertex := multidag.NewConcreteVertex()
-	vertex.AddAttribute("Hello", "There")
-	vertex.AddMultiOutpoint("blocks", "block", 800000, []int64{0, 1})
+	hLatestBlock, _ := cr.chainRead.LatestBlock()
+	if hLatestBlock.HeightSpecified() {
+		latestBlockHeight := hLatestBlock.Height()
+		blocksToShow := math.Min(2, float64(latestBlockHeight)+1)
+		blockHeights := []int64{}
+		for i := 0; i < int(blocksToShow); i++ {
+			blockHeights = append(blockHeights, int64(i))
+		}
+		vertex.AddMultiOutpoint("blocks", "block", latestBlockHeight+1, blockHeights)
+	}
 	return vertex
 }
 
 func (cr *ChainReader) GetBlockVertex(blockHeight int64) multidag.Vertex {
 	vertex := multidag.NewConcreteVertex()
-	vertex.AddAttribute("ThisIsA", "Block")
+	handle, _ := cr.handleCreator.BlockHandleByHeight(blockHeight)
+	block, _ := cr.chainRead.BlockInterface(handle)
+	nei, _ := block.NonEssentialInts()
+	for k, v := range *nei {
+		vertex.AddAttribute(k, strconv.Itoa(int(v)))
+	}
 	vertex.AddSingleInpoint("blockchain", "blockchain", 0, "blocks")
 	return vertex
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module pudding-server
 
 go 1.22
 
-require github.com/KitchenMishap/pudding-shed v0.0.0-20240828060836-d5cb2927fc66 // indirect
+require github.com/KitchenMishap/pudding-shed v0.0.1 // indirect
